2a4a7979681437c4532abaad4fd9c4e25dbc750a
JazzGlobal <waymer147@gmail.com>
2019-12-03 07:52:33 -0500

    Signup route will now sign a valid user up. Will deny signup if username or email is already in use.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express'),
     mongoose = require('mongoose'),
     passport = require('passport'),
+    bodyParser = require('body-parser'),
     LocalStrategy = require('passport-local'),
     User = require('./models/user'),
     app = express()
 
 // MONGOOSE CONFIG
 // Input correct <password>
-mongoose.connect('mongodb+srv://admin:<password>@cluster0-o2apy.mongodb.net/riot_api_server?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://admin:admin@cluster0-o2apy.mongodb.net/riot_api_server?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
 //PASSPORT CONFIG
 passport.serializeUser((user,done)=>{
@@ -31,6 +32,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // General Configuration
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -51,7 +54,7 @@ app.get('/logout', (req, res) => {
 
 app.get('/login', (req, res) => {
     if(req.user == null){
-        return res.render('login');
+        return res.render('login', {user: req.user});
     } else {res.redirect('/')}
 });
 
@@ -61,16 +64,20 @@ app.post('/login', passport.authenticate('local',{successRedirect: "/", failureR
 
 app.get('/signup', (req, res) => {
     if(req.user == null){
-        return res.render('signup');
+        return res.render('signup', {user: req.user});
     } else {res.redirect('/')}
 });
 
 app.post('/signup', (req, res) => {
-    var newUser = new User({username: req.body.username, name: req.body.name});
-    User.register(newUser, req.body.password, (err, user) => {
+    var newUser = new User(
+        {
+            username: req.body.username, 
+            email: req.body.email,
+        });
+    User.register(newUser, req.body.password ,(err, user) => {
         if(err){
             console.log(err);
-            return res.render('signup');
+            return res.render('signup', {user: user, err: err});
         }
         passport.authenticate('local')(req, res, ()=> {
             res.redirect('/');
